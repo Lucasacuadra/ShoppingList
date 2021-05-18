daf49bd37f0d45676cbd6ff96e4cfc57fc7ae7d0
Lucas Cuadra <lucasacuadra@gmail.com>
2021-05-18 20:26:49 +0200

    start git

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-app>
+    <v-main>
+      <ShoppingList />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+  import ShoppingList from './components/ShoppingList.vue';
+
+  export default {
+    components: {
+      ShoppingList
+    }
+  };
+</script>
+
+<style></style>
---
+++ src/components/ShoppingList.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="hello">
+    <h1>hello there</h1>
+    <v-btn
+      right
+      fixed
+      bottom
+      large
+      transition="slide-y-reverse-transition"
+      fab
+      dark
+      color="#03A9F4"
+      v-on="{ ...tooltip, ...dialog }"
+    >
+      <v-icon dark large>mdi-plus</v-icon>
+    </v-btn>
+  </div>
+</template>
+
+<script>
+  export default {};
+</script>
+
+<style scoped></style>
